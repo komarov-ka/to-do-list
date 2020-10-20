4016c9e22cacb9a99e13ea3dcc278df220144d84
Kirill_k <ya.komarovkirill@yandex.ru>
2020-10-20 20:58:32 +0300

    start developing the site structure and dividing elements into components. Added folder with icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import List from './components/List'
 
 function App() {
-  return <h1>123</h1>
+  return (
+    <div className="todo">
+      <div className="todo__sidebar">
+        <List />
+      </div>
+      <div className="todo__tasks"></div>
+    </div>
+  );
 }
 
 export default App;
